a55413458df4002af36c16b2c603992d4f6896c0
QILIGEER <qiligeer0313@gmail.com>
2017-05-30 21:10:06 +0900

    change test_same

--- test_same.sh
+++ test_same.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-￼tmp=/tmp/$$
+tmp=/tmp/$$
 
 ERROR_EXIT () {
-    echo "$1" >&2
-    rm -f $tmp-*
-   exit 1
+  echo "$1" >&2
+  rm -f $tmp-*
+  exit 1
 }
 #########
 # TEST1 #
 #########
 echo aaa bbb NG > $tmp-ans
-./same.py aaa bbb > $tmp-out || ERROR_EXIT "TEST1-1"
+./same.sh aaa bbb > $tmp-out || ERROR_EXIT "TEST1-1"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
 
 echo OK
